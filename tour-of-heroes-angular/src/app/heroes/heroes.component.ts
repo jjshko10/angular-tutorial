48a2d2a258350737d3a4d34a11c5db1f95b7d45f
unknown <staslukyanenko1995@gmail.com>
2023-03-18 12:46:38 +0300

    add: routing

--- tour-of-heroes-angular/src/app/heroes/heroes.component.ts
+++ tour-of-heroes-angular/src/app/heroes/heroes.component.ts
@@ -10,21 +10,15 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService,
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
+
   ngOnInit(): void {
     this.getHeroes();
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
